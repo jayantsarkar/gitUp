d2764430db1a8330309c7ebedc9344fa6c5be6fb
jayant <jayant.sarkar@outlook.com>
2013-12-12 17:08:27 +0530

    1st commit from desktop
    from desktop gitHub

---
+++ src/gitUp/GitUpPrint.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gitUp;
+
+/**
+ * @author jasarkar
+ *
+ */
+public class GitUpPrint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitUp Print in Console");
+	}
+}
---
+++ src/gitUp/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gitUp;
+
+/**
+ * @author jasarkar
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World gitUp");
+	}
+
+}
