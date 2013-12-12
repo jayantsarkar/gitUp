49952663f330785ddb1b6cf14c7dd4c1d835597c
jayantsarkar <jayant.sarkar@outlook.com>
2013-12-12 03:54:51 -0800

    Print.java

---
+++ src/gitUp/Print.java
@@ -0,0 +1,20 @@
+package gitUp;
+/**
+ * 
+ */
+
+/**
+ * @author jasarkar
+ *
+ */
+public class Print {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Print");
+	}
+
+}
